d1dc8ac742b4eb5925bf37620816c35b70ee63e0
babsonpython <112523357+babsonpython@users.noreply.github.com>
2022-10-30 18:41:54 -0400

    update

--- text_mining.py
+++ text_mining.py
@@ -1,18 +1,18 @@
+from pickle import TRUE
 import urllib.request
 from pprint import pprint
-import random
+from unicodedata import category
 import string
+import random
 
-def get_text():
+def get_text(url):
     """
-    This will be a function that gets text from the gutenberg project webpage.
+    This will be a function that gets text from the gutenberg project webpage and store it as a string of texts.
     """
-    url = 'https://www.gutenberg.org/files/2130/2130-0.txt'
-    with urllib.request.urlopen(url) as f:
-        response = urllib.request.urlopen(url)
-        data = response.read()  # a `bytes` object
-        text = data.decode('utf-8')
-    return text # for testing
+    response = urllib.request.urlopen(url)
+    data = response.read()  # a `bytes` object
+    text = data.decode('utf-8')
+    return text
 
 
 def process_file(filename, skip_header):
@@ -22,15 +22,16 @@ def process_file(filename, skip_header):
     returns: map from each word to the number of times it appears.
     """
     hist = {}
-    fp = open(filename, encoding='UTF8')
+    fp = open(filename, encoding = 'UTF-8')
 
     if skip_header:
         skip_gutenberg_header(fp)
 
+    # strippables = ''.join([chr(i) for i in range(sys.maxunicode) if category(chr(i)).startswith("P")]) #from professor's analyze_book solutions
     strippables = string.punctuation + string.whitespace
 
     for line in fp:
-        if line.startswith('*** END OF THE PROJECT GUTENBERG EBOOK UTOPIA ***'):
+        if line.startswith('*** END OF THE PROJECT GUTENBERG EBOOK THE COMPLETE WORKS OF WILLIAM SHAKESPEARE ***'):
             break
 
         line = line.replace('-', ' ')
@@ -50,21 +51,67 @@ def skip_gutenberg_header(fp):
     fp: open file object
     """
     for line in fp:
-        if line.startswith('*** START OF THE PROJECT GUTENBERG EBOOK UTOPIA ***'):
+        if line.startswith('*** START OF THE PROJECT GUTENBERG EBOOK THE COMPLETE WORKS OF WILLIAM SHAKESPEARE ***'):
             break
 
+def total_words(hist):
+    """Returns the total of the frequencies in a histogram."""
+    total = 0
+    for freq in hist.values():
+        total += freq
+    return total
+
+
+def different_words(hist):
+    """Returns the number of different words in a histogram."""
+    return len(hist)
+
+
+def most_common(hist, excluding_stopwords=False):
+    """Makes a list of word-freq pairs(tuples) in descending order of frequency.
+    hist: map from word to frequency
+    excluding_stopwords: a boolean value. If it is True, do not include any stopwords in the list.
+    returns: list of (frequency, word) pairs
+    """
+    most = list()
+    stopwords = open('stopwords.txt', encoding = 'utf8')
+    d = dict()
+    for line in stopwords:
+        for word in line.split():
+            d[word] = 1+ d.get(word,0)
+    stops = list(d.keys())
+    # print(stopwords)
+    for word, freq in hist.items():
+        if excluding_stopwords:
+            if word in stops:
+                continue
+
+        most.append((freq, word))
+    most.sort(reverse=True)
+    return most
+
+def random_word(hist):
+    """Chooses a random word from a histogram.
+    The probability of each word is proportional to its frequency.
+    """
+    t = []
+    for word, freq in hist.items():
+        t.extend([word] * freq)
+    return random.choice(t)
+
+
 def main():
-    text = get_text()
+    # text = get_text('https://www.gutenberg.org/cache/epub/100/pg100.txt')
     # print(text)
-    hist = process_file(text, skip_header=True)
-    print(hist)
+    hist = process_file('shakespeare.txt', skip_header=True)
+    # print(hist)
     # print('Total number of words:', total_words(hist))
     # print('Number of different words:', different_words(hist))
-    # print_most_common(hist, num =10)
 
-    # t = most_common(hist, excluding_stopwords=True)
+    # most = most_common(hist, excluding_stopwords=True)
+    # # print(most)
     # print('The most common words are:')
-    # for freq, word in t[0:20]:
+    # for freq, word in most[0:20]:
     #     print(word, '\t', freq)
 
     # words = process_file(text, skip_header=False)
@@ -74,9 +121,9 @@ def main():
     # for word in diff.keys():
     #     print(word, end=' ')
 
-    # print("\n\nHere are some random words from the book")
-    # for i in range(100):
-    #     print(random_word(hist), end=' ')
+    print("\n\nHere are some random words from the book: ")
+    for i in range(100):
+        print(random_word(hist), end=' ')
 
 
 if __name__ == '__main__':
